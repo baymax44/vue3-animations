88374eef6e541f96aab4204de6fff98f9e0cbd76
baymax44 <smartbay2021@outlook.com>
2020-03-12 21:33:48 -0400

    practice js hook with transitioin

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="about">
-    <h1>About</h1>
+    <transition
+      appear
+      name="fade"
+      @before-enter="beforeEnter"
+      @enter="enter"
+      @after-enter="afterEnter"
+      @before-leave="beforeLeave"
+      @leave="leave"
+      @after-leave="afterLeave"
+    >
+      <h1 v-if="showTitle">About</h1>
+    </transition>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Cum aperiam officia possimus delectus inventore quod quisquam culpa voluptas iusto, quae maiores quo dolorum, corporis laboriosam a dolore consequatur assumenda nam!</p>
@@ -8,10 +19,32 @@
 </template>
 
 <script>
+import { ref } from "vue"
 
 export default {
   setup() {
+    const showTitle = ref(true)
 
+    const beforeEnter = () => {
+      console.log("before enter")
+    }
+    const enter = () => {
+      
+    }
+    const afterEnter = () => {
+      
+    }
+    const beforeLeave = () => {
+      
+    }
+    const leave = () => {
+      
+    }
+    const afterLeave = () => {
+      
+    }
+
+    return { showTitle, beforeEnter, enter, afterEnter, beforeLeave, leave, afterLeave }
   }
 }
 </script>
@@ -20,5 +53,14 @@ export default {
   .about {
     max-width: 600px;
     margin: 20px auto;
+  }
+
+  .fade-enter-from,
+  .fade-leave-to {
+    opacity: 0;
+  }
+  .fade-enter-active,
+  .fade-leave-active {
+    transition: opacity 3s ease;
   }
 </style>
